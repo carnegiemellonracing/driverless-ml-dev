50a312901839d3ef92b9fd846152eb5c915dd1e7
Husain Raja <horaja@andrew.cmu.edu>
2026-01-17 14:41:41 -0500

    build: fixes config issues

--- bench/vars.mk
+++ bench/vars.mk
@@ -1,19 +1,17 @@
 # Paths
-ONNX      := ml_data/model_saves/11v_runs/yolo11n/weights/model.onnx
-ENGINE    := ml_data/model_saves/engines/yolo11n.fp16.engine
+ONNX      := ml_data/model_saves/26v_runs/yolo26n/best_26n.onnx
+ENGINE    := ml_data/model_saves/engines/yolo26n.fp16.engine
 IMG       := ml_data/fsoco_yolo/images/test/amz_amz_00097.jpg
 INFER_BIN := yolo-inference/build/inference
 
 # TensorRT build knobs (edit these)
-TRT_PREC  := --fp16                # or empty / --int8
+TRT_PREC  := --fp16             # mixed precision depending on hardware support 
 TRT_WS    := 2048                  # MiB
-TRT_SHAPES:= --minShapes=images:1x3x640x640 --optShapes=images:1x3x640x640 --maxShapes=images:1x3x640x640
-TRT_EXTRA :=                         # e.g. --verbose --useCudaGraph --noTF32
+TRT_EXTRA := --verbose                   # e.g. --verbose --useCudaGraph --noTF32
 
 # Nsight Systems knobs (edit these)
-NSYS_OUT  := ml_data/model_saves/profiles/yolo11n
+NSYS_OUT  := ml_data/model_saves/profiles/yolo26n
 NSYS_OPTS := --trace=cuda,nvtx,osrt \
-			 --accelerator-trace=tegra-accelerators \	# may have no effect
 			 --sample=process-tree \
              --cuda-memory-usage=true \
 			 --soc-metrics=true \
